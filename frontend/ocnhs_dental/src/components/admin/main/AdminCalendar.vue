225d12e08dfcccd92b1cf4714b699b40fc2c271b
chino-m <146500502+qlabsphofficial@users.noreply.github.com>
2025-04-07 01:09:00 +0800

    Minor Update
    Added Calendar

--- frontend/ocnhs_dental/src/components/admin/main/AdminCalendar.vue
+++ frontend/ocnhs_dental/src/components/admin/main/AdminCalendar.vue
@@ -1,12 +1,27 @@
-<template>
-      <div>
+<script setup>
+import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
+import { ref } from "vue"
 
+import 'vue-simple-calendar/dist/vue-simple-calendar.css'
+
+
+const showDate = ref(null)
+
+function setShowDate(d) {
+      this.showDate = d;
+}
+</script>
+
+<template>
+      <div class="w-11/12 h-11/12">
+            <CalendarView :show-date="showDate">
+                  <template #header="{ headerProps }">
+                          <CalendarViewHeader :header-props @input="setShowDate" />
+                  </template>
+            </CalendarView>
       </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
 
 <style scoped>
 
