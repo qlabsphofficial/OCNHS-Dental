cf7388a58ab59a6dae75fd516b21f87f92caf9a8
chino-m <146500502+qlabsphofficial@users.noreply.github.com>
2025-04-07 12:53:54 +0800

    minor update
    address patch

--- frontend/ocnhs_dental/src/address.js
+++ frontend/ocnhs_dental/src/address.js
@@ -1,5 +1,5 @@
 const local = 'http://127.0.0.1:8000'
 const host = 'https://ocnhs-dental-be.onrender.com'
-const current_address = local
+const current_address = host
 
 export default current_address
