cdf8aa7f347d14afaa70766886b0eca9a8dbf5f0
chino-m <146500502+qlabsphofficial@users.noreply.github.com>
2025-04-14 23:54:09 +0800

    Minor Update
    Test

--- frontend/ocnhs_dental/src/router/index.js
+++ frontend/ocnhs_dental/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 import LoginView from '@/views/LoginView.vue'
@@ -12,7 +12,7 @@ import ServiceView from '@/views/ServiceView.vue'
 import AdminSignUpView from '@/views/AdminSignUpView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
