c43a9b9f7493939195a38b90a6e63b38856c919a
chino-m <146500502+qlabsphofficial@users.noreply.github.com>
2025-04-15 05:03:36 +0800

    Minor Update
    Border

--- frontend/ocnhs_dental/src/components/client/main/ClientAppointmentComponent.vue
+++ frontend/ocnhs_dental/src/components/client/main/ClientAppointmentComponent.vue
@@ -14,14 +14,14 @@ onMounted(async() => {
 </script>
 
 <template>
-      <div class="flex flex-col lg:flex-row w-full h-full lg:h-10/12 lg:w-11/12 gap-5 p-10 border-2 rounded-md">
+      <div class="flex flex-col lg:flex-row w-full h-full lg:h-10/12 lg:w-11/12 gap-5 p-10 lg:border-2 rounded-md">
             <div class="flex flex-col justify-items items-center lg:items-start gap-3 w-full lg:w-4/12">
                   <h2 class="text-2xl lg:text-lg font-bold">Appointments</h2>
                   <!-- <div v-if="appointments.length == 0">
                         <p>No appointments yet.</p>
                   </div> -->
                   <div class="flex flex-col gap-7 h-full w-full overflow-y-scroll p-5">
-                        <div v-for="appointment of appointments" :key="appointment" class="flex flex-col border-2 rounded-md p-10 text-white bg-blue-400">
+                        <div v-for="appointment of appointments" :key="appointment" class="flex flex-col lg:border-2 rounded-md p-10 text-white bg-blue-400">
                               <h2 class="text-2xl">{{ appointment.appointment_type }}</h2>
                               <p>{{ formatDate(appointment.appointment_datetime) }}</p>
       
@@ -36,7 +36,7 @@ onMounted(async() => {
             <div class="flex flex-col gap-3 items-center justify-center lg:items-start lg:justify-start w-full lg:w-1/2">
                   <h2 class="text-2xl lg:text-lg font-bold mt-10 lg:mt-0">Appointment History</h2>
 
-                  <div v-for="appointmentRecord of appointments" :key="appointmentRecord" class="flex flex-col border-2 rounded-md p-10 w-full text-white bg-blue-400">
+                  <div v-for="appointmentRecord of appointments" :key="appointmentRecord" class="flex flex-col lg:border-2 rounded-md p-10 w-full text-white bg-blue-400">
                         <h2 class="text-2xl">{{ appointmentRecord.appointment_type }}</h2>
 
                         <div class="flex flex-row items-center justify-between w-full">
