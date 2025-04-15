be24f73b31e6339af92a6a62fbd86ae434f05651
Karl Resultan <146500502+qlabsphofficial@users.noreply.github.com>
2025-04-15 09:34:04 +0800

    Minor Update
    patch

--- frontend/ocnhs_dental/src/services/MedicalHistoryService.js
+++ frontend/ocnhs_dental/src/services/MedicalHistoryService.js
@@ -2,7 +2,16 @@ export async function retrieveMedicalHistory(id) {
       let medicalHistory = {}
 
       try {
-            const response = await fetch(`${current_address}/get_student_medical_history?student_id=${id}`)
+            const response = await fetch(`${current_address}/get_student_medical_history`, {
+                  method: 'POST',
+                  headers: {
+                  'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                        'student_id': id,
+                  })
+            })
+
             const data = await response.json()
 
             medicalHistory.student = data.student
